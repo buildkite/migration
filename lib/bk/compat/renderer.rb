28f8d989bc32525b467776187a9f2c15de24b174
Keith Pitt <me@keithpitt.com>
2019-12-19 10:34:42 +0800

    Support either YAML or JSON rendering of pipelines

--- lib/bk/compat/renderer.rb
+++ lib/bk/compat/renderer.rb
@@ -3,19 +3,32 @@ require "rouge"
 module BK
   module Compat
     class Renderer
+      module Format
+        YAML = :yaml
+        JSON = :json
+      end
+
       def initialize(structure)
         @structure = structure
       end
 
-      def render(colors: true)
-        yaml = JSON.parse(@structure.to_json).to_yaml
+      def render(colors: true, format: Format::YAML)
+        case format
+        when Format::YAML
+          text = JSON.parse(@structure.to_json).to_yaml
+          lexer = Rouge::Lexers::YAML.new
+        when Format::JSON
+          text = JSON.pretty_generate(@structure)
+          lexer = Rouge::Lexers::JSON.new
+        else
+          raise ArgumentError.new("Unknown format `#{format.inspect}`")
+        end
 
         if colors
           formatter = Rouge::Formatters::Terminal256.new(Rouge::Themes::Base16.new)
-          lexer = Rouge::Lexers::YAML.new
-          formatter.format(lexer.lex(yaml))
+          formatter.format(lexer.lex(text))
         else
-          yaml
+          text
         end
       end
     end
