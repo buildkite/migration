6ec2136467005daad6f895af21ced7caf454d6f8
Keith Pitt <me@keithpitt.com>
2020-03-02 13:35:15 +0800

    Added very basic (and barely working) Travis CI converter

--- lib/bk/compat.rb
+++ lib/bk/compat.rb
@@ -1,13 +1,15 @@
 require_relative "compat/renderer"
 require_relative "compat/pipeline"
 require_relative "compat/circleci"
+require_relative "compat/travisci"
 
 module BK
   module Compat
     VERSION = "0.1"
 
     PARSERS = [
-      BK::Compat::CircleCI
+      BK::Compat::CircleCI,
+      BK::Compat::TravsiCI
     ]
 
     def self.guess(text)
---
+++ lib/bk/compat/travisci.rb
@@ -0,0 +1,81 @@
+module BK
+  module Compat
+    class TravsiCI
+      require "yaml"
+
+      def self.name
+        "Travis CI"
+      end
+
+      def self.matches?(text)
+        keys = YAML.safe_load(text).keys
+        keys.include?("language")
+      end
+
+      def initialize(text)
+        @config = YAML.safe_load(text)
+      end
+
+      def parse
+        bk_pipeline = Pipeline.new
+
+        soft_fails = []
+        if allow_failures = @config.dig("matrix", "allow_failures")
+          allow_failures.each do |fc|
+            soft_fails << fc["rvm"]
+          end
+        end
+
+        script = []
+
+        if apt = @config.dig("addons", "apt")
+          if sources = apt["sources"]
+            sources.each do |s|
+              ppa = "ppa:#{s}"
+              script << "sudo add-apt-repository #{s.inspect}"
+            end
+            script << "sudo apt-get update"
+          end
+
+          if packages = apt["packages"]
+            packages.each do |pkg|
+              script << "sudo apt-get -q -y install #{pkg.inspect}"
+            end
+          end
+        end
+
+        if before_install = @config["before_install"]
+          [*before_install].each do |cmd|
+            script << cmd
+          end
+        end
+
+        script << @config.fetch("script")
+
+        @config.fetch("rvm").each do |rvm|
+          bk_step = BK::Compat::Pipeline::CommandStep.new(
+            label: ":travisci: #{rvm}",
+            commands: script
+          )
+
+          if soft_fails.include?(rvm)
+            bk_step.soft_fail = true
+          end
+
+          bk_step.plugins << BK::Compat::Pipeline::Plugin.new(
+            path: "docker#v3.3.0",
+            config: {
+              image: "ruby:#{rvm}",
+              workdir: "/buildkite-checkout"
+            }
+          )
+
+          bk_pipeline.steps << bk_step
+        end
+
+        bk_pipeline
+      end
+
+    end
+  end
+end
