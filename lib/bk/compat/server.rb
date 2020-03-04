2149ff093a4d6914da9a08f922e4e4ef743ee9e3
Keith Pitt <me@keithpitt.com>
2020-03-04 10:16:29 +0800

    Better error handling in the server version

--- lib/bk/compat/server.rb
+++ lib/bk/compat/server.rb
@@ -29,10 +29,20 @@ module BK
         parser_klass = BK::Compat.guess(contents)
 
         # Parse it and render it as YAML
-        body = parser_klass.new(contents).parse.render(colors: false, format: format)
+        begin
+          body = parser_klass.new(contents).parse.render(colors: false, format: format)
+          [200, { "Content-Type" => content_type }, StringIO.new(body)]
+        rescue BK::Compat::Error::NotSupportedError => e
+          error_message(500, e.message)
+        rescue => e
+          error_message(501, "Whoops! You found a bug! Please email keith@buildkite.com with a copy of the file you're trying to convert.")
+        end
+      end
 
-        # Yay, return it as YAML
-        [200, { "Content-Type" => content_type }, StringIO.new(body)]
+      private
+
+      def error_message(code, message)
+        [code, { "Content-Type" => "text/plain" }, StringIO.new("👎 #{message}\n")]
       end
     end
   end
