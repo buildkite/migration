a2226a0ceb51517add4ad75dd1d7e9195d917272
Keith Duncan <keith_duncan@me.com>
2020-04-16 19:40:42 +1000

    Disable colour by default when running in a pipe

--- app/lib/bk/compat/renderer.rb
+++ app/lib/bk/compat/renderer.rb
@@ -14,7 +14,7 @@ module BK
         @structure = structure
       end
 
-      def render(colors: true, format: Format::YAML)
+      def render(colors: STDOUT.tty?, format: Format::YAML)
         case format
         when Format::YAML
           text = JSON.parse(@structure.to_json).to_yaml
