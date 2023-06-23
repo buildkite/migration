f9b01d1215170051574365e5540f2b0974c3fb71
Matías A. Bellone <matias.bellone@buildkite.com>
2023-06-23 04:51:44 -0300

    Created new structure for plugins (easier versioning)

--- app/lib/bk/compat/pipeline/plugin.rb
+++ app/lib/bk/compat/pipeline/plugin.rb
@@ -2,15 +2,31 @@
 
 module BK
   module Compat
+    DEFAULT_VERSIONS = {
+      'docker' => 'v5.7.0',
+      'docker-compose' => 'v4.14.0',
+      'docker-login' => 'v2.1.0',
+      'ecr' => 'v2.7.0'
+    }.freeze
+
     # Plugin to use in a step
     class Plugin
-      def initialize(path:, config: nil)
-        @path = path
+      def initialize(name:, version: nil, config: nil)
+        @name = name
+        @version = version
         @config = config
       end
 
+      def path
+        base = @name
+        version = @version || !DEFAULT_VERSIONS.fetch(@name, nil)
+        base += "##{@version}" unless version.nil?
+
+        base
+      end
+
       def to_h
-        { @path => @config }
+        { path => @config }
       end
     end
   end
