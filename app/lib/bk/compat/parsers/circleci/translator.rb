97116d6873622bbab02c5a74d749419e71ae10c9
Matías A. Bellone <matias.bellone@buildkite.com>
2023-07-17 03:28:36 -0300

    Use methods instead of blocks for dispatch

--- app/lib/bk/compat/parsers/circleci/translator.rb
+++ app/lib/bk/compat/parsers/circleci/translator.rb
@@ -4,23 +4,19 @@ module BK
   module Compat
     # Dispatch pattern for step translation
     module StepTranslator
-      def register_step(key, &block)
+      def register_step(key, function)
         @known_steps ||= {} # utilize an instance variable that may not exist
 
         raise ArgumentError, "A translator for #{key} already exists" if @known_steps[key]
 
-        @known_steps[key] = block
+        @known_steps[key] = function
       end
 
       def translate_step(step_key, config, *args)
         @known_steps ||= {} # utilize an instance variable that may not exist
 
         translator = @known_steps[step_key]
-        if translator
-          translator.call(config, *args)
-        else
-          [" # #{step_key} not supported (yet)"]
-        end
+        translator&.call(config, *args) || [" # #{step_key} not supported (yet)"]
       end
     end
   end
