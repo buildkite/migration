859d37014077cb28af5f2137a766d08f71ee3e29
Matías Bellone <matias.bellone@readyset.co>
2023-09-06 22:07:13 -0300

    Fill out most of docker executor (with single container)

--- app/lib/bk/compat/parsers/circleci/executors.rb
+++ app/lib/bk/compat/parsers/circleci/executors.rb
@@ -6,46 +6,101 @@ module BK
     class CircleCI
       private
 
-      def parse_executor(name, config)
-        existing = config.slice('docker', 'machine', 'macos', 'windows')
-        
-        # only one of these must exist
-        raise "Invalid executor configuration #{config}" if existing.length != 1
+      def load_executor(name, config)
+        raise 'Duplicate executor name' if @executors.include?(name)
 
-        # parse the existing one
-        key = existing.keys.first
-        @executors[name] = BK::Compat::CommandStep.new(key: key).tap do |step|
+        type, ex_conf = get_executor(config)
+        raise "Invalid executor configuration #{config}" if key.nil?
+
+        @executors[name] = BK::Compat::CommandStep.new(key: "Executor #{type}").tap do |step|
           step.env = config.fetch('environment', {})
           step.commands << "cd #{config['working_directory']}" if config.include?('working_directory')
-          step.commands << '# shell is environment-dependent and should be configured in the agent' if config.include?('shell')
+          step.commands << '# shell should be configured in the agent' if config.include?('shell')
           step.agents['resource_class'] = config['resource_class'] if config.include?('resource_class')
-          step << send("executor_#{key}", existing[key])
+          step << parse_executor(type, ex_conf)
         end
       end
 
-      def executor_docker(_config)
-        BK::Compat::CommandStep.new().tap do |step|
+      def get_executor(config)
+        existing = config.slice('docker', 'machine', 'macos', 'windows')
+
+        # only one of these must exist
+        key = existing.length != 1 ? nil : existing.keys.first
+        # Hash[nil] == nil :)
+        [key, config[key]]
+      end
+
+      def parse_executor(type, config)
+        send("executor_#{type}", config)
+      end
+
+      def executor_docker(config)
+        raise 'Need to use Docker Compose for this :(' if config.length > 1
+
+        # assume we have a single configuration
+        config = config[0]
+
+        BK::Compat::CommandStep.new.tap do |step|
           step.agents['executor_type'] = 'docker'
+          plugin_config = {}.tap do |pgc|
+            pgc['image'] = config['image']
+            pgc['hostname'] = config['name'] if config.include?('name')
+            pgc['entrypoint'] = config['entrypoint'] if config.include?('entrypoint')
+            pgc['user'] = config['user'] if config.include?('user')
+
+            # TODO: use translate_run instead for this
+            pgc['command'] = config['command'] if config.include?('command')
+            pgc['environment'] = config['environment'] if config.include?('environment')
+          end
+          step.plugins << BK::Compat::Plugin.new(
+            name: 'docker',
+            config: plugin_config
+          )
+
+          if config.include?('auth')
+            step.plugins << BK::Compat::Plugin.new(
+              name: 'docker-login',
+              config: {
+                'username' => config['auth']['username'],
+                'password-env' => config['auth']['password'].ltrim('$')
+              }
+            )
+          end
+
+          if config.include?('aws-auth')
+            step.plugins << BK::Compat::Plugin.new(
+              name: 'ecr',
+              config: {
+                'login' => true,
+                'account-ids': config['image'].match(/^(\d+)\./)[1]
+              }
+            )
+            # TODO: handle OIDC or AWS credentials
+          end
         end
       end
 
       def executor_machine(config)
-        BK::Compat::CommandStep.new().tap do |step|
+        BK::Compat::CommandStep.new.tap do |step|
           step.agents['executor_type'] = 'machine'
           step.agents['executor_image'] = config['image']
-          step.commands = ['# Docker Layer Caching must be configured in the infra of the agent'] if config.include?('docker_layer_caching')
+          if config.include?('docker_layer_caching')
+            step.commands << '# Docker Layer Caching should be configured in the agent'
+          end
         end
       end
 
       def executor_macos(config)
-        BK::Compat::CommandStep.new().tap do |step|
+        BK::Compat::CommandStep.new.tap do |step|
           step.agents['executor_type'] = 'osx'
           step.agents['executor_xcode'] = config['xcode']
         end
       end
 
       def executor_windows(_config)
-        step.agents['executor_type'] = 'windows'
+        BK::Compat::CommandStep.new.tap do |step|
+          step.agents['executor_type'] = 'windows'
+        end
       end
     end
   end
