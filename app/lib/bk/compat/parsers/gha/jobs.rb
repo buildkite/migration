0d20d5a69aee125b1d271f72d6b0bc176235580b
Matías A. Bellone <matias.bellone@buildkite.com>
2023-10-08 15:58:18 -0300

    Support some other run configurations

--- app/lib/bk/compat/parsers/gha/jobs.rb
+++ app/lib/bk/compat/parsers/gha/jobs.rb
@@ -18,6 +18,36 @@ module BK
           bk_step.depends_on = Array(config['needs'])
         end
       end
+
+      def parse_step(step)
+        if step.include?('run')
+          BK::Compat::CommandStep.new(label: step['name']).tap do |cmd|
+            cmd.add_commands('# Shell is determined in the agent') if step.include?('shell')
+            cmd.add_commands('# timeouts are per-job, not step') if step.include?('timeout-minutes')
+            cmd.add_commands(
+              generate_command_string(
+                commands: step['run'],
+                env: step.fetch('env', {}),
+                workdir: step['working-directory']
+              )
+            )
+          end
+        else
+          "# step #{step} can not be translated just yet"
+        end
+      end
+
+      def generate_command_string(commands: [], env: {}, workdir: nil)
+        vars = env.map { |k, v| "#{k}='#{v}'" }
+        avoid_parens = vars.empty? && workdir.nil?
+        [
+          avoid_parens ? nil : '(',
+          workdir.nil? ? nil : "cd '#{workdir}'",
+          vars.empty? ? nil : vars.join(' '),
+          commands.lines(chomp: true),
+          avoid_parens ? nil : ')'
+        ].compact
+      end
     end
   end
 end
