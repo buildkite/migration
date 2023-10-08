8951f5f54a0d0a9ecbc47c4e1e89524520670f2b
Matías A. Bellone <matias.bellone@buildkite.com>
2023-10-08 13:53:12 -0300

    Support for env keyword in jobs

--- app/lib/bk/compat/parsers/gha/jobs.rb
+++ app/lib/bk/compat/parsers/gha/jobs.rb
@@ -8,7 +8,8 @@ module BK
         BK::Compat::CommandStep.new(
           label: ":github: #{name}",
           key: name,
-          depends_on: config.fetch('needs', [])
+          depends_on: config.fetch('needs', []),
+          env: config.fetch('env', {})
         ).tap do |bk_step|
           config['steps'].each { |step| bk_step << translate_step(step) }
           bk_step.agents.update(config.slice('runs-on'))
