89e4bb959fb5f3dc2ef4176536165721f75db6f7
Matías A. Bellone <matias.bellone@buildkite.com>
2023-10-11 18:33:32 -0300

    Refactored base command creation on parsing job

--- app/lib/bk/compat/parsers/gha/jobs.rb
+++ app/lib/bk/compat/parsers/gha/jobs.rb
@@ -5,6 +5,14 @@ module BK
     # GHA translation scaffolding
     class GitHubActions
       def parse_job(name, config)
+        generate_base_step(name, config).tap do |bk_step|
+          config['steps'].each { |step| bk_step << translate_step(step) }
+          bk_step.agents.update(config.slice('runs-on'))
+          bk_step.depends_on = Array(config['needs'])
+        end
+      end
+
+      def generate_base_step(name, config)
         BK::Compat::CommandStep.new(
           label: ":github: #{name}",
           key: name,
@@ -12,12 +20,9 @@ module BK
           env: config.fetch('env', {}),
           timeout_in_minutes: config['timeout-minutes'],
           soft_fail: config['continue-on-error']
-        ).tap do |bk_step|
-          config['steps'].each { |step| bk_step << translate_step(step) }
-          bk_step.agents.update(config.slice('runs-on'))
-          bk_step.depends_on = Array(config['needs'])
-        end
+        )
       end
+
     end
   end
 end
