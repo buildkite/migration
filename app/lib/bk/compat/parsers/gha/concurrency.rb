1b442f99b8bc763f2621f91de12a38a8e411c994
James Sammut <juma2791@gmail.com>
2023-10-16 08:34:45 +1100

    Normalize def name, using `add_commands` instead of `<<`,

--- app/lib/bk/compat/parsers/gha/concurrency.rb
+++ app/lib/bk/compat/parsers/gha/concurrency.rb
@@ -4,15 +4,15 @@ module BK
   module Compat
     class GitHubActions
       def set_concurrency(bk_step, config)
-        group, cancel_in_progress = obtain_concurrency(config['concurrency']) 
+        group, cancel_in_progress = normalize_concurrency(config['concurrency']) 
         # If cancel-in-progress was defined in the steps' concurrency hash
-        bk_step << ["# The concurrency setting of `cancel-in-progress: #{cancel_in_progress}` for this step is not supported. Please use Cancel Intermediate Builds."] if !cancel_in_progress.nil?
+        bk_step.add_commands("# cancel-in-progress in Buildkite is the pipeline setting Cancel Intermediate Builds") unless cancel_in_progress.nil?
         # Set concurrency/concurrency_group
         bk_step.concurrency = 1 
         bk_step.concurrency_group = group
       end
   
-      def obtain_concurrency(concurrency)
+      def normalize_concurrency(concurrency)
         case concurrency
         when String
           [concurrency, nil]
