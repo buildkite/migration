beace5e2615e5a8c2433f745d5b473d22e88f236
Ben McNicholl <git@benmcnicholl.com>
2023-10-17 15:48:42 +1100

    Make the validator loop over all files

--- app/spec/lib/bk/compat/gha_spec.rb
+++ app/spec/lib/bk/compat/gha_spec.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
-
 require_relative '../../../../lib/bk/compat'
 
 RSpec.describe BK::Compat::GitHubActions do
- 
-  context 'basic.yaml' do 
-    let(:subject) do
-      gha_content = File.open("examples/gha/basic.yaml").read 
-      BK::Compat::GitHubActions.new(gha_content).parse() 
-    end
-    
-    it 'does stuff' do       
-      actual = JSON.parse(subject.to_h.to_json).to_yaml
-      expect(actual).to match_snapshot('basic.yaml')
-    end
-  end  
+  context 'it runs a snapshot test on each example' do 
+    directory_path = 'examples/gha'
+
+    Dir.glob("#{directory_path}/*.yaml").each do |file|
+      next if File.directory?(file)
+
+      file_name = File.basename(file)
+
+      it "compares the generated pipeline for #{file_name} to the original" do
+        gha_content = File.read(file)
+        parsed_content = BK::Compat::GitHubActions.new(gha_content).parse()
+
+        actual = JSON.parse(parsed_content.to_h.to_json).to_yaml
+        expect(actual).to match_snapshot(file_name)
+      end
+    end  
+  end
 end
