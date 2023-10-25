e41687e99de74811559529278937f05959558dc0
Matías A. Bellone <matias.bellone@buildkite.com>
2023-10-25 14:16:57 -0300

    Simplify prepending of commands and handle missing optional settings

--- app/lib/bk/compat/parsers/gha/services.rb
+++ app/lib/bk/compat/parsers/gha/services.rb
@@ -7,20 +7,22 @@ module BK
       def set_services(bk_step, services)
         return nil if services.nil?
 
-        bk_step.prepend_commands('# It may be a good idea to use docker compose to start up job-specific services')
-        bk_step.prepend_commands(*docker_cmds(services).compact)
+        cmds = ['# It may be a good idea to use docker compose to start up job-specific services']
+        cmds.concat(docker_cmds(services).flatten)
+        bk_step.prepend_commands(cmds.compact)
 
-        bk_step.add_commands("docker stop #{services.keys}")
+        bk_step.add_commands("docker stop #{services.keys.join(' ')}")
       end
 
       def docker_cmds(services)
         services.map do |name, config|
           # All options map directly to a docker `create` command
-          envs = config['env'].map { |k, v| "--env \"#{k}='#{v}'\"" }.join(' ')
-          ports = config['ports'].map { |p| "--expose '#{p}'" }.join(' ')
-          vols = config['volumes'].map { |v| "--volume '#{v}'" }.join(' ')
+          envs = config.fetch('env', {}).map { |k, v| "--env \"#{k}='#{v}'\"" }.join(' ')
+          ports = config.fetch('ports', []).map { |p| "--expose '#{p}'" }.join(' ')
+          vols = config.fetch('volumes', []).map { |v| "--volume '#{v}'" }.join(' ')
 
           [
+            "echo 'Starting #{name} service'",
             config.include?('credentials') ? '# Warning: you should use the docker-login plugin to authenticate' : nil,
             "docker start --rm -d --name #{name} #{envs} #{ports} #{vols} #{config['options']} #{config['image']}"
           ]
