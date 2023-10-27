197cbeef69a71d92a6c5d64043a4a57f1fe2f27f
Jarryd Tilbrook <jarryd@buildkite.com>
2023-10-27 12:05:25 +0800

    Fix matrix numbers as strings

--- app/lib/bk/compat/parsers/gha/matrix.rb
+++ app/lib/bk/compat/parsers/gha/matrix.rb
@@ -1,15 +1,30 @@
 # frozen_string_literal: true
 
+require_relative '../../pipeline/step'
+
 module BK
   module Compat
     class GitHubActions
       def set_matrix(bk_step, config)
-        matrix = config['strategy']['matrix']
-        matrix.map { |key, value| [key, value.map(&:to_s)] }.to_h
+        matrix = convert_to_string(config['strategy']['matrix'])
         bk_step.matrix = {
           'setup' => matrix,
           'adjustments' => []
         }
+      end
+
+      def convert_to_string(value)
+        case value
+        when Hash
+          value.transform_values! { |elem| convert_to_string(elem) }
+        when Array
+          value.map! { |elem| convert_to_string(elem) }
+        when Numeric
+          "#{value}"
+        else
+          # if we don't know how to do this, do nothing
+          value
+        end
       end
     end
   end
