a049e05c4a9068b3f82697253f1c58cb0abadcb3
Jarryd Tilbrook <jarryd@buildkite.com>
2023-10-27 09:22:39 +0800

    Revert "Update app/lib/bk/compat/parsers/gha/matrix.rb"
    This reverts commit 6dab8b063f6cf54b3e29a464dfd3c320acdefdba.

--- app/lib/bk/compat/parsers/gha/matrix.rb
+++ app/lib/bk/compat/parsers/gha/matrix.rb
@@ -4,7 +4,8 @@ module BK
   module Compat
     class GitHubActions
       def set_matrix(bk_step, config)
-        matrix = config['strategy']['matrix'].transform_values(&:to_s)
+        matrix = config['strategy']['matrix']
+        matrix.map { |key, value| [key, value.map(&:to_s)] }.to_h
         bk_step.matrix = {
           'setup' => matrix,
           'adjustments' => []
