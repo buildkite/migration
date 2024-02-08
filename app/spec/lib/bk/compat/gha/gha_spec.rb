e59674afb6ef9d518b481f728076d625313793f2
James Sammut <juma2791@gmail.com>
2024-02-08 14:17:41 +1100

    Adjusted GHA/Circle spec to distinct directories, snapshot regeneration

--- app/spec/lib/bk/compat/gha/gha_spec.rb
+++ app/spec/lib/bk/compat/gha/gha_spec.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-require_relative '../../../../lib/bk/compat/parsers/circleci'
+require_relative '../../../../../lib/bk/compat'
 
-RSpec.describe BK::Compat::CircleCI do
-  let(:circleci) { BK::Compat::CircleCI }
-
+RSpec.describe BK::Compat::GitHubActions do
   context 'it runs a snapshot test on each example' do
-    directory_path = 'examples/circleci'
+    directory_path = 'examples/github-actions'
 
     Dir.glob("#{directory_path}/*").each do |file|
       next if File.directory?(file)
 
       it "compares the generated pipeline for #{file} to the original" do
         gha_content = File.read(file)
-        parsed_content = circleci.new(gha_content).parse
+        parsed_content = BK::Compat::GitHubActions.new(gha_content).parse
 
         actual = parsed_content.render(colors: false)
         expect(actual).to match_snapshot(file)
