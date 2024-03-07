072f85299e931ab9082a629539eb544717b2d130
James Sammut <juma2791@gmail.com>
2024-03-07 13:39:19 +1100

    Tweaks

--- app/lib/bk/compat/pipeline/step.rb
+++ app/lib/bk/compat/pipeline/step.rb
@@ -33,6 +33,7 @@ module BK
 
       def to_h
         { block: @key, key: @key, depends_on: @depends_on }.tap do |h| 
+          # rename conditional to if (a reserved word as an attribute or instance variable is complicated)
           h[:if] = @conditional unless @conditional.nil?
         end
       end
