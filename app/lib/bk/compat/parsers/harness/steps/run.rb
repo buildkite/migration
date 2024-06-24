543002d35186812333298f29a8cf60cb16e143f2
James Sammut <juma2791@gmail.com>
2024-06-24 11:18:27 +1000

    Image translation base

--- app/lib/bk/compat/parsers/harness/steps/run.rb
+++ app/lib/bk/compat/parsers/harness/steps/run.rb
@@ -8,12 +8,27 @@ module BK
       # Implementation of native step translation
       class Translator
         def translate_run(name:, identifier:, spec:, **_rest)
-          BK::Compat::CommandStep.new(
+          cmd = BK::Compat::CommandStep.new(
             label: name,
             key: identifier,
             commands: [spec['command']]
           )
+          cmd << translate_image(spec['image'])      
+          cmd
         end
+
+        def translate_image(image) 
+          p image
+          return nil if image.nil?
+
+          BK::Compat::Plugin.new(
+            name: 'docker',
+            config: {
+              'image' => image,
+            }.compact
+          )
+        end
+        
       end
     end
   end
