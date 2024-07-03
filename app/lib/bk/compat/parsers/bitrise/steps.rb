b14722508c43faa67aa1b603f0244bb7cdb76377
James Sammut <juma2791@gmail.com>
2024-07-03 16:10:28 +1000

    Initial change-workdir implementation, activesupport gem, spec

--- app/lib/bk/compat/parsers/bitrise/steps.rb
+++ app/lib/bk/compat/parsers/bitrise/steps.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require 'active_support//core_ext/object/blank'
+
 module BK
   module Compat
     module BitriseSteps
       # Implementation of Bitrise step translations
       class Translator
-        VALID_STEP_TYPES = %w[git-clone script].freeze
+        VALID_STEP_TYPES = %w[change-workdir git-clone script].freeze
 
         def matcher(type, _config)
           VALID_STEP_TYPES.include?(type.downcase)
@@ -13,6 +15,22 @@ module BK
 
         def translator(type, config)
           send("translate_#{type.downcase.gsub('-', '_')}", config)
+        end
+
+        def translate_change_workdir(config)
+          generate_change_workdir_command(config['path'], config['is_create_path'])
+        end
+
+        def generate_change_workdir_command(path, create_directory)
+          if create_directory && path.present?
+            [
+              "mkdir #{path} && cd #{path}"
+            ]
+          elsif !create_directory && path.present?
+            [
+              "cd #{path}"
+            ]
+          end
         end
 
         def translate_git_clone(_config)
