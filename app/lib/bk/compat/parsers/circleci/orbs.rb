ccc41f51fcdaf301b74b5dd8d4c71faf8ebd9fb9
phanichowdary9988 <phaninaidu1993@gmail.com>
2025-01-17 13:01:17 -0600

    Add install-docker command

--- app/lib/bk/compat/parsers/circleci/orbs.rb
+++ app/lib/bk/compat/parsers/circleci/orbs.rb
@@ -1,7 +1,40 @@
+# frozen_string_literal: true
+
 module BK
   module Compat
     module CircleCISteps
+      # Generic orb steps/jobs (that states it is not supported)
+      class GenericOrb
+        def initialize(key)
+          @prefix = key
+        end
+
+        def matcher(orb, _conf)
+          orb.start_with?("#{@prefix}/")
+        end
+
+        def translator(action, _config)
+          [
+            "# #{action} is part of orb #{@prefix} which is not supported and should be translated by hand"
+          ]
+        end
+      end
+
+      # Docker orb specific implementation
       class DockerOrb
+        DOCKER_INSTALL_PARAMS = {
+          'install-dir' => {
+            'type' => 'string',
+            'description' => 'Directory in which to install Docker binaries',
+            'default' => '/usr/local/bin'
+          },
+          'version' => {
+            'type' => 'string',
+            'description' => 'Version of Docker to install',
+            'default' => 'latest'
+          }
+        }.freeze
+
         def matcher(orb, _conf)
           orb.start_with?('docker/')
         end
@@ -11,7 +44,11 @@ module BK
           when 'docker/build'
             translate_docker_build(config)
           when 'docker/install'
-            translate_docker_install(config)
+            translate_docker_install
+          when 'docker/install-docker'
+            validate_and_translate_install_docker(config || {})
+          when 'docker/push'
+            translate_docker_push(config)
           else
             ["# Unsupported docker orb action: #{action}"]
           end
@@ -20,16 +57,71 @@ module BK
         private
 
         def translate_docker_build(config)
+          image_tag = "#{config['image']}:#{config['tag'] || 'latest'}"
           [
-            "docker build -t #{config['image']}:#{config['tag']} ."
+            "# No need to setup remote docker, use the host docker",
+            "docker build -t #{image_tag} ."
           ]
         end
 
-        def translate_docker_install(config)
+        def translate_docker_install
           [
             "echo '~~~ Installing Docker'",
             "curl -fsSL https://get.docker.com -o get-docker.sh",
             "sh get-docker.sh"
+          ]
+        end
+
+        def validate_and_translate_install_docker(config)
+          # Suggestion: Ensure Docker and necessary tools are pre-installed in the agent
+          # If no special parameters are provided, use simple installation
+          has_custom_dir = config.key?('install-dir') && config['install-dir'] != DOCKER_INSTALL_PARAMS['install-dir']['default']
+          has_specific_version = config.key?('version') && config['version'] != DOCKER_INSTALL_PARAMS['version']['default']
+
+          if !has_custom_dir && !has_specific_version
+            return [
+              "echo '~~~ Installing Docker'",
+              "curl -fsSL https://get.docker.com -o get-docker.sh",
+              "sh get-docker.sh"
+            ]
+          end
+
+          # Advanced installation with custom parameters
+          install_dir = config['install-dir'] || DOCKER_INSTALL_PARAMS['install-dir']['default']
+          version = config['version'] || DOCKER_INSTALL_PARAMS['version']['default']
+          
+          commands = []
+          commands << "#!/usr/bin/env bash"
+          commands << ""
+          commands << "if [[ $EUID == 0 ]]; then export SUDO=\"\"; else export SUDO=\"sudo\"; fi"
+          commands << ""
+
+          if has_custom_dir
+            commands << "# Creating custom installation directory"
+            commands << "$SUDO mkdir -p #{install_dir}"
+            commands << ""
+          end
+
+          if has_specific_version
+            commands << "# Installing specific Docker version: #{version}"
+            commands << "VERSION=#{version}"
+            commands << "curl -fsSL https://download.docker.com/linux/static/stable/x86_64/docker-${VERSION}.tgz -o docker.tgz"
+            commands << "tar xzvf docker.tgz"
+            commands << "$SUDO cp docker/* #{install_dir}/"
+            commands << "rm -rf docker docker.tgz"
+            commands << ""
+          else
+            commands << "curl -fsSL https://get.docker.com -o get-docker.sh"
+            commands << "sh get-docker.sh"
+          end
+
+          commands
+        end
+
+        def translate_docker_push(config)
+          image_tag = "#{config['image']}:#{config['tag'] || 'latest'}"
+          [
+            "docker push #{image_tag}"
           ]
         end
       end
--- app/spec/lib/bk/compat/circleci_spec.rb
+++ app/spec/lib/bk/compat/circleci_spec.rb
@@ -20,4 +20,95 @@ RSpec.describe BK::Compat::CircleCI do
       end
     end
   end
+  describe 'docker/install-docker parameters' do
+    let(:docker_orb) { BK::Compat::CircleCISteps::DockerOrb.new }
+
+    context 'with no parameters' do
+      let(:config) { {} }
+
+      it 'uses simple installation' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to include("echo '~~~ Installing Docker'")
+        expect(result).to include('curl -fsSL https://get.docker.com -o get-docker.sh')
+        expect(result).to include('sh get-docker.sh')
+        expect(result.size).to eq(3)  # Should only have these 3 commands
+      end
+    end
+
+    context 'with install-dir parameter' do
+      let(:config) { { 'install-dir' => '/custom/path' } }
+
+      it 'creates custom directory and installs there' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to include('$SUDO mkdir -p /custom/path')
+        expect(result).to include('if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi')
+      end
+
+      it 'uses default installation method with custom directory' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to include('curl -fsSL https://get.docker.com -o get-docker.sh')
+        expect(result).to include('sh get-docker.sh')
+      end
+    end
+
+    context 'with version parameter' do
+      let(:config) { { 'version' => 'v20.10.23' } }
+
+      it 'installs specific version' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to include('VERSION=v20.10.23')
+        expect(result).to include('curl -fsSL https://download.docker.com/linux/static/stable/x86_64/docker-${VERSION}.tgz -o docker.tgz')
+        expect(result).to include('tar xzvf docker.tgz')
+        expect(result).to include('rm -rf docker docker.tgz')
+      end
+
+      it 'uses default installation directory' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to include('$SUDO cp docker/* /usr/local/bin/')
+      end
+    end
+
+    context 'with both version and install-dir parameters' do
+      let(:config) { { 'version' => 'v20.10.23', 'install-dir' => '/opt/docker' } }
+
+      it 'combines both custom directory and version installation' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to include('$SUDO mkdir -p /opt/docker')
+        expect(result).to include('VERSION=v20.10.23')
+        expect(result).to include('$SUDO cp docker/* /opt/docker/')
+      end
+    end
+
+    context 'with default parameter values' do
+      let(:config) { { 'install-dir' => '/usr/local/bin', 'version' => 'latest' } }
+
+      it 'uses simple installation when defaults are specified explicitly' do
+        result = docker_orb.translator('docker/install-docker', config)
+        
+        expect(result).to eq([
+          "echo '~~~ Installing Docker'",
+          'curl -fsSL https://get.docker.com -o get-docker.sh',
+          'sh get-docker.sh'
+        ])
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'ignores unknown parameters' do
+        result = docker_orb.translator('docker/install-docker', { 'unknown' => 'value' })
+        
+        expect(result).to eq([
+          "echo '~~~ Installing Docker'",
+          'curl -fsSL https://get.docker.com -o get-docker.sh',
+          'sh get-docker.sh'
+        ])
+      end
+    end
+  end  
 end
