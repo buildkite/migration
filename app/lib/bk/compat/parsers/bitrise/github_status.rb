a2d65057ab24d1cffead4890158b1b88b09ef16f
phanichowdary9988 <phaninaidu1993@gmail.com>
2025-02-28 09:46:56 -0600

    SUP_2406_github_status_translation

--- app/lib/bk/compat/parsers/bitrise/github_status.rb
+++ app/lib/bk/compat/parsers/bitrise/github_status.rb
@@ -6,32 +6,15 @@ module BK
       # Handles the translation of github-status
       class Translator
         def translate_github_status(inputs)
-          context = inputs['context'] || inputs['status_identifier'] || 'continuous-integration/buildkite'
-          warnings = generate_warnings_for_unsupported_options(inputs.keys)
-
-          github_status = {
-            'context' => context
-          }
-
-          step = BK::Compat::CommandStep.new
-
-          step.notify = [{ 'github_commit_status' => github_status }]
-          step.add_commands(warnings) unless warnings.empty?
-
-          step
-        end
-
-        private
-
-        def generate_warnings_for_unsupported_options(input_keys)
-          supported_options = ['status_identifier']
-          warnings = []
-          input_keys.each do |key|
-            next if supported_options.include?(key)
-
-            warnings << "# Warning: The '#{key}' option is not supported in Buildkite and will be ignored."
+          context = inputs['status_identifier'] || 'continuous-integration/buildkite'
+          unless inputs.empty?
+            warnings = '# Only the status_identifier option is supported for github status translation'
           end
-          warnings
+
+          BK::Compat::CommandStep.new(
+            notify: [{ 'github_commit_status' => { 'context' => context } }],
+            commands: [warnings].compact
+          )
         end
       end
     end
